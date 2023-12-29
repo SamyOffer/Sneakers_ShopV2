5628e88a5b65b128e379498b80e2663c8d80fbe1
SamyOffer <samyoffer@gmail.com>
2023-12-30 00:23:18 +0100

    tous fonctione youhouuu (bon j'ai pas fais vrmt le payement mais on peut rediriger avec stripe)

--- client/src/components/CartPageV2.jsx
+++ client/src/components/CartPageV2.jsx
@@ -6,6 +6,7 @@ import data003 from "../database/data003.json";
 const CartPageV2 = () => {
   // Access the parameters from the URL
   const [cart, setCart] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
 
   useEffect(() => {
     // demande le panier a lutilisateur
@@ -39,34 +40,73 @@ const CartPageV2 = () => {
     fetchCart();
   }, []);
 
-  const DisplayShoes = () => {
-    return cart.map((shoe) => {
-      let imageURL = null;
+  useEffect(() => {
+    // Calculate total price separately
+    let totalPrice = 0;
+    cart.forEach((shoe) => {
       for (const sneakerKey in data003) {
         const sneaker = data003[sneakerKey];
-        if (parseInt(sneaker.id)=== parseInt(shoe.id)) {
-          imageURL = sneaker.imageURL;
+        if (parseInt(sneaker.id) === parseInt(shoe.id)) {
+          totalPrice += sneaker.price * shoe.quantity;
           break; // Sortir de la boucle une fois que l'objet est trouvé
         }
       }
-      
-      return (
-        <div key={shoe._id}>
-          <img src={imageURL} alt={imageURL} />
-          <p>Shoe ID: {shoe.id}</p>
-          <p>Selected Size: {shoe.size}</p>
-          <p>Quantity: {shoe.quantity}</p>
-        </div>
-      );
     });
+    setTotalPrice(totalPrice);
+  }, [cart]);
+
+  const DisplayShoes = () => {
+    // Verifier si le panier est vide
+    if (cart.length === 0) {
+      return <p className="text-3xl text-center mt-20">Your Cart is empty !</p>;
+    }
+
+    return (
+      <div className="flex flex-col items-start">
+        {cart.map((shoe) => {
+          let sneakerData = null;
+          for (const sneakerKey in data003) {
+            const sneaker = data003[sneakerKey];
+            if (parseInt(sneaker.id) === parseInt(shoe.id)) {
+              sneakerData = sneaker;
+              break; // Sortir de la boucle une fois que l'objet est trouvé
+            }
+          }
+
+          return (
+            <div key={shoe._id} className="flex items-center mb-4">
+              <img
+                src={sneakerData.imageURL}
+                alt={sneakerData.imageURL}
+                className="w-40 h-40 ml-20 mt-10 rounded"
+              />
+              <div>
+                <p className="text-lg font-bold ml-5"> {sneakerData.name}</p>
+                <p className="text-base ml-5 ">Selected Size: {shoe.size}</p>
+                <p className="text-base ml-5">Quantity: {shoe.quantity}</p>
+                <p className="text-base ml-5">Price: {sneakerData.price * shoe.quantity}€</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
   };
-  
-  
 
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <DisplayShoes />
+      <div className="flex-1 flex">
+        <div className="flex-1">
+          <DisplayShoes />
+        </div>
+        <div className="flex-0 p-4 mt-auto pr-48">
+          <p className="text-2xl font-bold mb-20">Total Price: {totalPrice}€</p>
+          <button className="bg-black text-white px-12 py-2 rounded border-2 border-white">
+            Validate my cart
+          </button>
+        </div>
+      </div>
       <Footer />
     </div>
   );
